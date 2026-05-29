00ccecb24187a208eabc4b739299fe0cf5e71eb0
Karunya <karunyatummala@gmail.com>
2026-05-29 09:16:07 +0530

    best coder file added

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import os
 print(os.getcwd())
-<<<<<<< HEAD
 print(os.chdir("C:\Users\Dj.IT\Desktop\Python"))
 print(os.get_blocking())
-=======
-
-print(os.listdir()) #lists all the files in the current directory
->>>>>>> feature_python
